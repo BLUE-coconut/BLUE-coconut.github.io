7ea248c77f6eea7e35c27a6c58eac2a18fed23b4
BLUE-coconut <1367012021@qq.com>
2025-08-01 11:37:49 +0800

    完成biography页面迁移和移动端适配

--- src/views/BiographyView.vue
+++ src/views/BiographyView.vue
@@ -1,5 +1,211 @@
 <template>
-  
+  <div class="my-box">
+    <div class="container">
+      <div class="two-column resume">
+        <section class="resume__section resume__header">
+          <div class="resume__content">
+            <h1>Haolan He (何昊蓝)</h1>
+            <div class="info-item">
+              <span class="info-label"><i class="fa fa-location-arrow"></i></span>
+              <span class="info-text">Hangzhou Dianzi University, No.1158, the 2nd Street in Baiyang Street, Hangzhou Economic Development Zone, Hangzhou, China</span>
+            </div>
+            <div class="info-item">
+              <span class="info-label"><i class="fa fa-envelope"></i></span>
+              <span class="info-text">haolanhe@hdu.edu.cn</span>
+            </div>
+          </div>
+        </section>
+        <div class="resume__columns">
+          <div class="resume__main">
+            <section class="resume__section resume__summary">
+              <div class="resume__content">
+                <div class="resume__section-title">
+                  <i class="fa fa-pencil-square-o"></i>
+                  <h2>Professional Summary</h2>
+                </div>
+                <div class="other">
+                  <div class="other-info">
+                    <p>
+                      Computer science learner and resercher. Researching interests include artificial intellegence, computer vision and human computer interaction.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </section>
+            <section class="resume__section resume__summary">
+              <div class="resume__content">
+                <div class="resume__section-title">
+                  <i class="fa fa-mortar-board"></i>
+                  <h2>Education</h2>
+                </div>
+                <div class="xp-item">
+                  <div class="xp-job">
+                    HangzhouDianzi University, Zhuoyue Honors College
+                  </div>
+                  <span>Undergraduate</span>
+                  <div class="xp-date" style="float: right; font-weight: 400;">Sept. 2021 – June 2025</div>
+                  <div class="xp-detail">
+                    <ul>
+                      <li>Major: Intelligent science and technology</li>
+                      <li>GPA: 4.79/5.0</li>
+                      <li>Rank: 1/114</li>
+                    </ul>
+                  </div>
+                </div>
+                <div class="xp-item">
+                  <div class="xp-job">
+                    Zhejiang University
+                  </div>
+                  <span>Master</span>
+                  <div class="xp-date" style="float: right; font-weight: 400;">Pre-admission for Recommended Exemption</div>
+                  <div class="xp-detail">
+                    <ul>
+                      <li>Major: Artificial intellegence</li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </section>
+            <section class="resume__section resume__experience">
+              <div class="resume__content">
+                <div class="resume__section-title">
+                  <i class="fa fa-briefcase"></i>
+                  <h2>Researches</h2>
+                </div>
+                <div class="xp-item">
+                  <div class="xp-job">
+                    Interactive Fusion and Correlation Network for Three-Modal Images Few-Shot Semantic Segmentation
+                  </div>
+                  <div class="xp-detail">
+                    <b>Haolan He</b>; Xianguo Dong; Xiaofei Zhou; Bo Wang; Jiyong Zhang;
+                    <ul>
+                      <li>
+                        Published in IEEE Signal Processing Letters, 2024 
+                        [<a href="https://ieeexplore.ieee.org/document/10669915/authors#authors">Paper</a>] 
+                        [<a href="https://github.com/BLUE-coconut/Fewshot-VDT">Code</a>]
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                <div class="xp-item">
+                  <div class="xp-job">
+                    Efficient Object Detection Based on Multimodal Remote Sensing Images 
+                    <small>, "Xinmiao" Talents Plan of Zhejiang Province</small>
+                  </div>
+                  <div class="xp-detail">
+                    <b>Project Leader</b>
+                    <div class="xp-date" style="float: right; font-weight: 400;">ongoing</div>
+                  </div>
+                </div>
+              </div>
+            </section>
+            <section class="resume__section resume__experience">
+              <div class="resume__content">
+                <div class="resume__section-title">
+                  <i class="fa fa-trophy"></i>
+                  <h2>Scholarships & Awards</h2>
+                </div>
+                <div class="xp-item">
+                  <ul>
+                    <li class="list-item">
+                      National Scholarships,
+                      <span class="date">2024</span>
+                    </li>
+                    <li class="list-item">
+                      Zhejiang Provincial Government Scholarship,
+                      <span class="date">2023</span>
+                    </li>
+                    <li class="list-item">
+                      Zhejiang Provincial Government Scholarship,
+                      <span class="date">2022</span>
+                    </li>
+                    <li class="list-item">
+                      Outstanding Individual of the 19th Asian Games,
+                      <span class="date">2024</span>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </section>
+          </div>
+          <div class="resume__side">
+            <section class="resume__section resume__skills">
+              <div class="resume__content">
+                <div class="resume__section-title">
+                  <i class="fa fa-align-center"></i>
+                  <h2>Skills</h2>
+                </div>
+                <div class="resume__text">
+                  <div class="extra">
+                    <div class="extra-info">Python</div>
+                    <div class="extra-details">
+                      <div class="extra-details__progress" style="width:95%"></div>
+                    </div>
+                  </div>
+                  <div class="extra">
+                    <div class="extra-info">Deeplearning<br/><small>ML · DL · CV · NLP</small></div>
+                    <div class="extra-details">
+                      <div class="extra-details__progress" style="width:80%"></div>
+                    </div>
+                  </div>
+                  <div class="extra">
+                    <div class="extra-info">AI framework<br/><small>Pytorch · Tensflow · Mindspore · Paddle</small></div>
+                    <div class="extra-details">
+                      <div class="extra-details__progress" style="width:75%"></div>
+                    </div>
+                  </div>
+                  <div class="extra">
+                    <div class="extra-info">HTML</div>
+                    <div class="extra-details">
+                      <div class="extra-details__progress" style="width:90%"></div>
+                    </div>
+                  </div>
+                  <div class="extra">
+                    <div class="extra-info">CSS</div>
+                    <div class="extra-details">
+                      <div class="extra-details__progress" style="width:75%"></div>
+                    </div>
+                  </div>
+                  <div class="extra">
+                    <div class="extra-info">JavaScript<br/><small>React · Vue</small></div>
+                    <div class="extra-details">
+                      <div class="extra-details__progress" style="width:50%"></div>
+                    </div>
+                  </div>
+                  <div class="extra">
+                    <div class="extra-info">IOS Development</div>
+                    <div class="extra-details">
+                      <div class="extra-details__progress" style="width:15%"></div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </section>
+            <section class="resume__section resume__languages">
+              <div class="resume__content">
+                <div class="resume__section-title">
+                  <i class="fa fa-globe"></i>
+                  <h2>Languages</h2>
+                </div>
+                <div class="extra">
+                  <div class="extra-info">Chinese <small>(native)</small></div>
+                  <div class="extra-details">
+                    <div class="extra-details__progress" style="width:100%"></div>
+                  </div>
+                </div>
+                <div class="extra">
+                  <div class="extra-info">English <small>(IELTS 7.5)</small></div>
+                  <div class="extra-details">
+                    <div class="extra-details__progress" style="width:80%"></div>
+                  </div>
+                </div>
+              </div>
+            </section>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -7,5 +213,349 @@
 </script>
 
 <style scoped>
+.my-box {
+  margin-top: 75px;
+  height: fit-content;
+  width: 100%;
+  padding: 0 15px;
+}
 
+.my-box .container {
+  width: 70%;
+  max-width: 1200px;
+  margin: 0 auto;
+  position: relative;
+  padding: 20px;
+  border-radius: 30px;
+  background: rgba(255, 255, 255, 1);
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+}
+
+/* Resume styles */
+.resume .resume__header,
+.resume .resume__section {
+  font-family: 'Open Sans', sans-serif;
+  font-size: 1.1em;
+}
+
+.resume .resume__header {
+  padding: 3em 4em 0;
+}
+
+.resume .resume__section {
+  margin-bottom: 2em;
+}
+
+.resume .resume__section:last-child {
+  padding-bottom: 0;
+}
+
+.resume .resume__section-title {
+  display: flex;
+  align-items: center;
+  margin-bottom: 1.2em;
+}
+
+.resume .resume__section-title > i {
+  margin-right: 0.63em;
+  font-size: 1.14em;
+  background-color: #5695cd;
+  color: #fff;
+  border: 0.25em solid #aacae6;
+  border-radius: 50%;
+  width: 2.51em;
+  height: 2.51em;
+  min-width: 2.51em;
+  min-height: 2.51em;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  line-height: 1.6;
+  flex-shrink: 0;
+}
+
+.resume .resume__section-title > h2 {
+  margin-top: 0;
+  font-size: 1.7em;
+}
+
+.resume .resume__columns {
+  overflow: hidden;
+  padding: 4em;
+  padding-top: 0;
+}
+
+.resume .resume__main {
+  float: left;
+  width: 75%;
+  padding-right: 6em;
+}
+
+.resume .resume__side {
+  float: left;
+  width: 25%;
+}
+
+.resume .other-info p {
+  font-size: 1.1em;
+}
+
+.resume .other-info p > b {
+  color: #555;
+}
+
+.resume .info-item {
+  margin-bottom: 0.2em;
+  font-weight: 300;
+  font-size: 1.05em;
+}
+
+.resume .info-item:last-child {
+  margin-bottom: 0;
+}
+
+.resume .info-label {
+  display: inline-block;
+  padding-right: 0.63em;
+  font-size: 1.14em;
+  min-width: 2.19em;
+  text-align: center;
+}
+
+.resume .info-label i {
+  color: #5695cd;
+}
+
+.resume .xp-item {
+  margin-bottom: 1em;
+}
+
+.resume .xp-item:last-child {
+  margin-bottom: 0;
+}
+
+.resume .xp-job {
+  font-size: 1.3em;
+  font-weight: 600;
+  line-height: 1.25;
+}
+
+.resume .xp-job span,
+.resume .xp-job small {
+  font-size: 1em;
+  font-weight: 400;
+}
+
+.resume .xp-job small {
+  font-size: 0.9em;
+}
+
+.resume .xp-date {
+  font-size: 0.8em;
+  margin-top: 0.2em;
+  margin-bottom: 0.2em;
+  color: #5695cd;
+}
+
+.resume .list-item {
+  font-size: 1.25em;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.resume .date {
+  font-weight: 400;
+  font-size: 1em;
+  color: #5695cd;
+  margin-left: auto;
+}
+
+.resume .xp-detail {
+  margin-top: 5px;
+  font-size: 1.15em;
+}
+
+/* 列表对齐样式 */
+.resume ul {
+  padding-left: 1.2em;
+  margin-left: 0;
+}
+
+.resume li {
+  margin-bottom: 0.3em;
+}
+
+.resume .extra {
+  margin-bottom: 2em;
+}
+
+.resume .extra:last-child {
+  margin-bottom: 0;
+}
+
+.resume .extra-info small {
+  color: #666;
+  display: inline-block;
+  font-size: 0.7em;
+}
+
+.resume .extra-details,
+.resume .extra-details__progress {
+  border-radius: 6px;
+}
+
+.resume .extra-details {
+  margin-top: 0.5em;
+  background-color: #d1d9e1;
+  width: 100%;
+  height: 5px;
+  position: relative;
+}
+
+.resume .extra-details__progress {
+  background-color: #5695cd;
+  height: 5px;
+  position: absolute;
+  top: 0;
+  left: 0;
+}
+
+.resume .lang-item {
+  margin-bottom: 2em;
+}
+
+.resume .lang-item:last-child {
+  margin-bottom: 0;
+}
+
+.resume .lang-label {
+  width: 8em;
+}
+
+/* Mobile responsive styles */
+@media (max-width: 1100px) {
+  .my-box .container {
+    width: 80%;
+    padding: 15px;
+  }
+  
+  .resume .resume__header {
+    padding: 2em 2em 0;
+  }
+  
+  .resume .resume__columns {
+    padding: 2em;
+    padding-top: 0;
+  }
+  
+  .resume .resume__main {
+    padding-right: 3em;
+  }
+}
+
+@media (max-width: 768px) {
+  .my-box .container {
+    width: 95%;
+    padding: 10px;
+  }
+  
+  .resume .resume__header {
+    padding: 1.5em 1em 0;
+  }
+  
+  .resume .resume__columns {
+    padding: 1em;
+    padding-top: 0;
+  }
+  
+  .resume .resume__main {
+    float: none;
+    width: 100%;
+    padding-right: 0;
+    margin-bottom: 2em;
+  }
+  
+  .resume .resume__side {
+    float: none;
+    width: 100%;
+  }
+  
+  .resume .resume__section-title > h2 {
+    font-size: 1.3em;
+  }
+  
+  .resume .resume__section-title > i {
+    width: 2.2em;
+    height: 2.2em;
+    min-width: 2.2em;
+    min-height: 2.2em;
+    font-size: 1em;
+    flex-shrink: 0;
+  }
+  
+  .resume .info-item {
+    margin-bottom: 0.5em;
+  }
+  
+  .resume .info-text {
+    display: block;
+    margin-top: 0.2em;
+  }
+}
+
+@media (max-width: 480px) {
+  .my-box {
+    padding: 0 10px;
+  }
+  
+  .my-box .container {
+    width: 100%;
+    padding: 8px;
+    border-radius: 15px;
+  }
+  
+  .resume .resume__header {
+    padding: 1em 0.5em 0;
+  }
+  
+  .resume .resume__columns {
+    padding: 0.5em;
+    padding-top: 0;
+  }
+  
+  .resume .resume__section-title > h2 {
+    font-size: 1.2em;
+  }
+  
+  .resume .resume__section-title > i {
+    width: 2em;
+    height: 2em;
+    min-width: 2em;
+    min-height: 2em;
+    font-size: 0.9em;
+    margin-right: 0.5em;
+    flex-shrink: 0;
+  }
+  
+  .resume .xp-job {
+    font-size: 1em;
+  }
+  
+  .resume .xp-date {
+    float: none !important;
+    margin-top: 0.3em;
+    display: block;
+  }
+  
+  .resume .list-item {
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 0.2em;
+  }
+  
+  .resume .date {
+    margin-left: 0;
+  }
+}
 </style>
